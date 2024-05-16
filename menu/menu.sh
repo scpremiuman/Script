545a688171b3a6f5f2f810d2b1c9637ab6ffc7d0
tegalvpn <vsugp508@gmail.com>
2024-05-16 21:27:51 +0700

    Add files via upload

--- menu/menu.sh
+++ menu/menu.sh
@@ -443,7 +443,7 @@ echo -e " $COLOR1│$NC ${WH}[${COLOR1}02${WH}]${NC} ${COLOR1}• ${WH}VMESS    
 echo -e " $COLOR1│$NC ${WH}[${COLOR1}03${WH}]${NC} ${COLOR1}• ${WH}VLESS     ${WH}[${COLOR1}Menu${WH}]   ${NC} $COLOR1│${NC}  ${WH}[${COLOR1}09${WH}]${NC} ${COLOR1}• ${WH}UPDATE     ${WH}[${COLOR1}Menu${WH}] $COLOR1│${NC}"   
 echo -e " $COLOR1│$NC ${WH}[${COLOR1}04${WH}]${NC} ${COLOR1}• ${WH}TROJAN    ${WH}[${COLOR1}Menu${WH}]   ${NC} $COLOR1│${NC}  ${WH}[${COLOR1}10${WH}]${NC} ${COLOR1}• ${WH}SYSTEM     ${WH}[${COLOR1}Menu${WH}] $COLOR1│${NC}" 
 echo -e " $COLOR1│$NC ${WH}[${COLOR1}05${WH}]${NC} ${COLOR1}• ${WH}NOOBZVPNS ${WH}[${COLOR1}Menu${WH}]   ${NC} $COLOR1│${NC}  ${WH}[${COLOR1}11${WH}]${NC} ${COLOR1}• ${WH}BACKUP     ${WH}[${COLOR1}Menu${WH}] $COLOR1│${NC}" 
-echo -e " $COLOR1│$NC ${WH}[${COLOR1}06${WH}]${NC} ${COLOR1}• ${WH}BOT AKUN  ${WH}[${COLOR1}Menu${WH}]   ${NC} $COLOR1│${NC}  ${WH}[${COLOR1}12${WH}]${NC} ${COLOR1}• ${WH}REBOOT     ${WH}[${COLOR1}Menu${WH}] $COLOR1│${NC}" 
+echo -e " $COLOR1│$NC ${WH}[${COLOR1}06${WH}]${NC} ${COLOR1}• ${WH}BOT AKUN  ${WH}[${COLOR1}Menu${WH}]   ${NC} $COLOR1│${NC}  ${WH}[${COLOR1}12${WH}]${NC} ${COLOR1}• ${WH}DELLXP     ${WH}[${COLOR1}Menu${WH}] $COLOR1│${NC}" 
 echo -e " $COLOR1│$NC ${WH}[${COLOR1}22${WH}]${NC} ${COLOR1}• ${WH}RUNNING   ${WH}[${COLOR1}Menu${WH}]   ${NC} $COLOR1│${NC}  ${WH}[${COLOR1}23${WH}]${NC} ${COLOR1}• ${WH}RESTART    ${WH}[${COLOR1}Menu${WH}] $COLOR1│${NC}"
 echo -e " $COLOR1╰════════════════════════════════════════════════════════╯${NC}"
 if [ "$Isadmin" = "ON" ]; then
@@ -560,6 +560,160 @@ wget https://raw.githubusercontent.com/scpremiuman/Script/main/menu/install-up.s
 chmod +x install_up.sh
 ./install_up.sh
 }
+function xp(){
+    clear
+    echo "SUCCESSFULLY DELETE ALL EXPIRED ACCOUNT "
+    echo "---------------------------------------------"
+cd
+echo 1 > /proc/sys/vm/drop_caches
+data=( `cat /etc/xray/ssh | grep '^###' | cut -d ' ' -f 2 | sort | uniq`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
+pass=$(grep -w "^### $user" "/etc/xray/ssh" | cut -d ' ' -f 4 | sort | uniq)
+exp=$(grep -w "^### $user" "/etc/xray/ssh" | cut -d ' ' -f 3 | sort | uniq)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" -le "0" ]]; then
+sed -i "/^### $user $exp $pass/d" /etc/xray/ssh
+if getent passwd $user > /dev/null 2>&1; then
+userdel $user > /dev/null 2>&1
+fi
+rm /home/vps/public_html/ssh-$user.txt >/dev/null 2>&1
+rm /etc/xray/sshx/${user}IP >/dev/null 2>&1
+rm /etc/xray/sshx/${user}login >/dev/null 2>&1
+fi
+done
+data=( `cat /etc/xray/config.json | grep '^#vmg' | cut -d ' ' -f 2 | sort | uniq`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
+exp=$(grep -w "^#vmg $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
+uuid=$(grep -w "^#vmg $user" "/etc/xray/config.json" | cut -d ' ' -f 4 | sort | uniq)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" -le "0" ]]; then
+if [ ! -e /etc/vmess/akundelete ]; then
+echo "" > /etc/vmess/akundelete
+fi
+clear
+echo "### $user $exp $uuid" >> /etc/vmess/akundelete
+sed -i "/^#vmg $user $exp/,/^},{/d" /etc/xray/config.json
+sed -i "/^#vm $user $exp/,/^},{/d" /etc/xray/config.json
+rm -f /etc/xray/$user-tls.json /etc/xray/$user-none.json
+rm /home/vps/public_html/vmess-$user.txt >/dev/null 2>&1
+rm /etc/vmess/${user}IP >/dev/null 2>&1
+rm /etc/vmess/${user}login >/dev/null 2>&1
+fi
+done
+data=( `cat /etc/xray/config.json | grep '^#vlg' | cut -d ' ' -f 2 | sort | uniq`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
+exp=$(grep -w "^#vlg $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
+uuid=$(grep -w "^#vlg $user" "/etc/xray/config.json" | cut -d ' ' -f 4 | sort | uniq)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" -le "0" ]]; then
+if [ ! -e /etc/vless/akundelete ]; then
+echo "" > /etc/vless/akundelete
+fi
+clear
+echo "### $user $exp $uuid" >> /etc/vless/akundelete
+sed -i "/^#vlg $user $exp/,/^},{/d" /etc/xray/config.json
+sed -i "/^#vl $user $exp/,/^},{/d" /etc/xray/config.json
+rm /home/vps/public_html/vless-$user.txt >/dev/null 2>&1
+rm /etc/vless/${user}IP >/dev/null 2>&1
+rm /etc/vless/${user}login >/dev/null 2>&1
+fi
+done
+data=( `cat /etc/xray/config.json | grep '^#trg' | cut -d ' ' -f 2 | sort | uniq`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
+exp=$(grep -w "^#trg $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
+uuid=$(grep -w "^#trg $user" "/etc/xray/config.json" | cut -d ' ' -f 4 | sort | uniq)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" -le "0" ]]; then
+if [ ! -e /etc/trojan/akundelete ]; then
+echo "" > /etc/trojan/akundelete
+fi
+clear
+echo "### $user $exp $uuid" >> /etc/trojan/akundelete
+sed -i "/^#tr $user $exp/,/^},{/d" /etc/xray/config.json
+sed -i "/^#trg $user $exp/,/^},{/d" /etc/xray/config.json
+rm /home/vps/public_html/trojan-$user.txt >/dev/null 2>&1
+rm /etc/trojan/${user}IP >/dev/null 2>&1
+rm /etc/trojan/${user}login >/dev/null 2>&1
+fi
+done
+systemctl restart xray
+data=( `cat /etc/xray/config.json | grep '^#ssg' | cut -d ' ' -f 2 | sort | uniq`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
+exp=$(grep -w "^#ssg $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" -le "0" ]]; then
+sed -i "/^#ssg $user $exp/,/^},{/d" /etc/xray/config.json
+sed -i "/^#ssg $user $exp/,/^},{/d" /etc/xray/config.json
+fi
+done
+systemctl restart xray
+data=( `cat /etc/xray/config.json | grep '^#ss' | cut -d ' ' -f 2 | sort | uniq`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
+exp=$(grep -w "^#ss $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" -le "0" ]]; then
+sed -i "/^#ss $user $exp/,/^},{/d" /etc/xray/config.json
+sed -i "/^#ss $user $exp/,/^},{/d" /etc/xray/config.json
+fi
+done
+systemctl restart xray
+hariini=`date +%d-%m-%Y`
+cat /etc/shadow | cut -d: -f1,8 | sed /:$/d > /tmp/expirelist.txt
+totalaccounts=`cat /tmp/expirelist.txt | wc -l`
+for((i=1; i<=$totalaccounts; i++ ))
+do
+tuserval=`head -n $i /tmp/expirelist.txt | tail -n 1`
+username=`echo $tuserval | cut -f1 -d:`
+userexp=`echo $tuserval | cut -f2 -d:`
+userexpireinseconds=$(( $userexp * 86400 ))
+tglexp=`date -d @$userexpireinseconds`
+tgl=`echo $tglexp |awk -F" " '{print $3}'`
+while [ ${#tgl} -lt 2 ]
+do
+tgl="0"$tgl
+done
+while [ ${#username} -lt 15 ]
+do
+username=$username" "
+done
+bulantahun=`echo $tglexp |awk -F" " '{print $2,$6}'`
+todaystime=`date +%s`
+if [ $userexpireinseconds -ge $todaystime ] ;
+then
+:
+else
+userdel --force $username
+fi
+done
+systemctl reload ssh
+
+    read -n 1 -s -r -p "Press any key to back on menu"
+    menu
+}
 echo -e "$COLOR1╭═════════════════════════════════════════════════════════╮${NC}"
 echo -e "$COLOR1│$NC ${WH} ❈ Versi   ${NC}: ${WH}$(cat /opt/.ver) Version${NC}$COLOR1"
 echo -e "$COLOR1│$NC ${WH} ❈ Client ${NC} : ${WH}$author${NC}"
@@ -580,7 +734,7 @@ case $opt in
 09 | 9) clear ; m-update ;;
 10 | 10) clear ; m-system ;;
 11 | 11) clear ; m-backup;;
-12 | 12) clear ; reboot ;;
+12 | 12) clear ; xp ;;
 13 | 13) clear ; $ressee ;;
 14 | 14) clear ; key ;;
 22 | 22) clear ; running;;
